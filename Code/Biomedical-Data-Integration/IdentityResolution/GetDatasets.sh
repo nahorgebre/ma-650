dacdcbde074778b4232f0891506ca0fda5f26282
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-09-12 15:01:15 +0200

    Ajust Identity Resolution

--- Code/Biomedical-Data-Integration/IdentityResolution/GetDatasets.sh
+++ Code/Biomedical-Data-Integration/IdentityResolution/GetDatasets.sh
@@ -32,32 +32,44 @@ wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/ident
 
 # Brain
 mkdir -p data/goldstandard/Brain/Brain_2_mart_export_brain
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Brain/Brain_2_mart_export_brain/train.csv
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Brain/Brain_2_mart_export_brain/test.csv
 
 mkdir -p data/goldstandard/Brain/mart_export_brain_2_all_gene_disease_pmid_associations
 
 
 # Cerebellum
 mkdir -p data/goldstandard/Cerebellum/Cerebellum_2_mart_export_cerebellum
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Cerebellum/Cerebellum_2_mart_export_cerebellum/train.csv
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Cerebellum/Cerebellum_2_mart_export_cerebellum/test.csv
 
 mkdir -p data/goldstandard/Cerebellum/mart_export_cerebellum_2_all_gene_disease_pmid_associations
 
 
 # Heart
 mkdir -p data/goldstandard/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk/train.csv
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk/test.csv
 
 mkdir -p data/goldstandard/Heart/Heart_Ensembl_NCBI_Crosswalk_2_all_gene_disease_pmid_associations
 
 mkdir -p data/goldstandard/Heart/mart_export_heart_2_Heart_Ensembl_NCBI_Crosswalk
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Heart/mart_export_heart_2_Heart_Ensembl_NCBI_Crosswalk/train.csv
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Heart/mart_export_heart_2_Heart_Ensembl_NCBI_Crosswalk/test.csv
 
 
 # Kidney
 mkdir -p data/goldstandard/Kidney/Kidney_2_mart_export_kidney
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Kidney/Kidney_2_mart_export_kidney/train.csv
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Kidney/Kidney_2_mart_export_kidney/test.csv
 
 mkdir -p data/goldstandard/Kidney/mart_export_kidney_2_all_gene_disease_pmid_associations
 
 
 # Liver
 mkdir -p data/goldstandard/Liver/Liver_2_mart_export_liver
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Liver/Liver_2_mart_export_liver/train.csv
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Liver/Liver_2_mart_export_liver/test.csv
 
 mkdir -p data/goldstandard/Liver/mart_export_liver_2_all_gene_disease_pmid_associations
 
@@ -66,3 +78,5 @@ mkdir -p data/goldstandard/Liver/mart_export_liver_2_all_gene_disease_pmid_assoc
 mkdir -p data/goldstandard/Testis/mart_export_testis_2_all_gene_disease_pmid_associations
 
 mkdir -p data/goldstandard/Testis/Testis_2_mart_export_testis
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Testis/Testis_2_mart_export_testis/train.csv
+https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/goldstandard/Testis/Testis_2_mart_export_testis/test.csv
