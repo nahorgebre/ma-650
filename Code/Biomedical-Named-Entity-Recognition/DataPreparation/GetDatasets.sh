47e90e1e8acf90f5dfaae93601ae657cdc93b8e3
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-10-09 17:39:43 +0200

    Update Patent Parser

--- Code/Biomedical-Named-Entity-Recognition/DataPreparation/GetDatasets.sh
+++ Code/Biomedical-Named-Entity-Recognition/DataPreparation/GetDatasets.sh
@@ -1,23 +1,23 @@
 mkdir -p data/input/parser
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/parser/title.tsv -O data/input/parser/title.tsv
 
-mkdir -p data/input/CHEMDNER_TEST_TEXT
+mkdir -p data/input/CHEMDNERgoldstandard/CHEMDNER_TEST_TEXT
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/CHEMDNER_TEST_TEXT/Readme_CHEMDNER_test.pdf -O data/input/CHEMDNERgoldstandard/CHEMDNER_TEST_TEXT/Readme_CHEMDNER_test.pdf
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/CHEMDNER_TEST_TEXT/chemdner_patents_test_background_text_release.txt -O data/input/CHEMDNERgoldstandard/CHEMDNER_TEST_TEXT/chemdner_patents_test_background_text_release.txt
 
-mkdir -p data/input/GPRO_development_baseline
+mkdir -p data/input/CHEMDNERgoldstandard/GPRO_development_baseline
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/GPRO_development_baseline/README_gpro_development_baseline.txt -O data/input/CHEMDNERgoldstandard/GPRO_development_baseline/README_gpro_development_baseline.txt
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/GPRO_development_baseline/gpro_official_dev_lookup_baseline.eval -O data/input/CHEMDNERgoldstandard/GPRO_development_baseline/gpro_official_dev_lookup_baseline.eval
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/GPRO_development_baseline/gpro_official_dev_lookup_baseline.tsv -O data/input/CHEMDNERgoldstandard/GPRO_development_baseline/gpro_official_dev_lookup_baseline.tsv
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/GPRO_development_baseline/grpo_official_train_lookup_list.tsv -O data/input/CHEMDNERgoldstandard/GPRO_development_baseline/grpo_official_train_lookup_list.tsv
 
-mkdir -p data/input/gpro_development_set
+mkdir -p data/input/CHEMDNERgoldstandard/gpro_development_set
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/gpro_development_set/README_gpro_development.txt -O data/input/CHEMDNERgoldstandard/gpro_development_set/README_gpro_development.txt
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/gpro_development_set/chemdner_gpro_gold_standard_development.tsv -O data/input/CHEMDNERgoldstandard/gpro_development_set/chemdner_gpro_gold_standard_development.tsv
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/gpro_development_set/chemdner_gpro_gold_standard_development_eval.tsv -O data/input/CHEMDNERgoldstandard/gpro_development_set/chemdner_gpro_gold_standard_development_eval.tsv
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/gpro_development_set/chemdner_patents_development_text.txt -O data/input/CHEMDNERgoldstandard/gpro_development_set/chemdner_patents_development_text.txt
 
-mkdir -p data/input/gpro_training_set_v02
+mkdir -p data/input/CHEMDNERgoldstandard/gpro_training_set_v02
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/gpro_training_set_v02/README_gpro_train.txt -O data/input/CHEMDNERgoldstandard/gpro_training_set_v02/README_gpro_train.txt
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/gpro_training_set_v02/chemdner_gpro_gold_standard_train_eval_v02.tsv -O data/input/CHEMDNERgoldstandard/gpro_training_set_v02/chemdner_gpro_gold_standard_train_eval_v02.tsv
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-preparation/input/CHEMDNERgoldstandard/gpro_training_set_v02/chemdner_gpro_gold_standard_train_v02.tsv -O data/input/CHEMDNERgoldstandard/gpro_training_set_v02/chemdner_gpro_gold_standard_train_v02.tsv
