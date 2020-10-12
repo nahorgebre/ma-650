4f92c857d8fade2f15ae61e027b2c35e54869641
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-10-13 01:31:04 +0200

    Update Program.cs

--- Code/Biomedical-Named-Entity-Recognition/ExtractPatentData/Program.cs
+++ Code/Biomedical-Named-Entity-Recognition/ExtractPatentData/Program.cs
@@ -7,44 +7,16 @@ namespace ExtractPatentData
     {
         static void Main(string[] args)
         {
-            /*
-            FileStream stream;
-            StreamWriter writer;
-            TextWriter consoleOut = Console.Out;
 
-            try
-            {
-                Directory.CreateDirectory("./logs");
-                string fileName = "./logs/ExtractPatentData.log";
-                File.Delete(fileName);
-                stream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
-                writer = new StreamWriter(stream);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Cannot open ExtractPatentData.log for writing");
-                Console.WriteLine(ex.Message);
-                return; 
-            }
-
-            Console.SetOut(writer);
-            */
-
-            //Patent.getPatentNumbersByYear();
+            Patent.getPatentNumbersByYear();
             
-            //ParserPFTAPS.run();
-            //ParserPG2.run();
+            ParserPFTAPS.run();
+            ParserPG2.run();
             ParserIPG2.run();
 
-            //Output.run();
+            Output.run();
 
             //AWSupload.run();
-
-            /*
-            Console.SetOut(consoleOut);
-            writer.Close();
-            stream.Close();
-            */
 
         }
     }
