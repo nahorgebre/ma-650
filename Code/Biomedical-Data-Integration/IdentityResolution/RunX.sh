649d297bde9078874a84e016a13c8d0fd3db1a78
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-10-13 13:42:04 +0200

    Create RunX.sh

---
+++ Code/Biomedical-Data-Integration/IdentityResolution/RunX.sh
@@ -0,0 +1,13 @@
+mvn compile
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.Cerebellum.mart_export_cerebellum_2_gene2pubtatorcentral.Run"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.Heart.Heart_Ensembl_NCBI_Crosswalk_2_gene2pubtatorcentral.Run"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.Kidney.mart_export_kidney_2_gene2pubtatorcentral.Run"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.Liver.mart_export_liver_2_gene2pubtatorcentral.Run"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.Testis.mart_export_testis_2_gene2pubtatorcentral.Run"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.Publication.Run"
