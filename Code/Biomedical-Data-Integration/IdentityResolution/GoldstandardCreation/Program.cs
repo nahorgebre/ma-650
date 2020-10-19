7b2d6f489633b4638fd5af1ed8b5dbd500ad8f48
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-10-19 16:09:14 +0200

    Update BioMed-DI

--- Code/Biomedical-Data-Integration/IdentityResolution/GoldstandardCreation/Program.cs
+++ Code/Biomedical-Data-Integration/IdentityResolution/GoldstandardCreation/Program.cs
@@ -7,6 +7,7 @@ namespace GoldstandardCreation
         static void Main(string[] args)
         {
 
+            /*
             Brain.run();
             Cerebellum.run();
             Heart.run();
@@ -18,7 +19,15 @@ namespace GoldstandardCreation
 
             Output.run();
 
-            //AWSupload.run();
+            AWSupload.run();
+            */
+
+            for (int i = 36; i <= 100; i++)
+            {
+                string x = "wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/input/gene2pubtatorcentral_" + i + "_dt.tsv -O data/input/Publication/gene2pubtatorcentral_" + i + "_dt.tsv";
+
+                Console.WriteLine(x);
+            }
 
         }
 
