85b5806271198ebdb4f336f1eeccf462d998da82
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-10-20 02:19:59 +0200

    Update UploadToS3.java

--- Code/Biomedical-Data-Integration/IdentityResolution/src/main/java/genes/IdentityResolution/AWS/UploadToS3.java
+++ Code/Biomedical-Data-Integration/IdentityResolution/src/main/java/genes/IdentityResolution/AWS/UploadToS3.java
@@ -7,6 +7,9 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+
 public class UploadToS3 {
     
     public static void main( String[] args ) throws Exception
@@ -15,16 +18,6 @@ public class UploadToS3 {
 
     public static void uploadFileToS3(String file_path, String bucket_name, String key_name) {
 
-        s3Client = new AmazonS3Client();
-
-        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucket_name);
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
-        try {
-            s3.putObject(bucket_name, key_name, new File(file_path));
-        } catch (AmazonServiceException e) {
-            System.err.println(e.getErrorMessage());
-            System.exit(1);
-        }
     }
 
 }
