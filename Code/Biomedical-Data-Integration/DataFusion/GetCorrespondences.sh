cb43343af0c430ea2c013140921d94c6e979e068
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-10-20 16:26:38 +0200

    Update GetCorrespondences.sh

--- Code/Biomedical-Data-Integration/DataFusion/GetCorrespondences.sh
+++ Code/Biomedical-Data-Integration/DataFusion/GetCorrespondences.sh
@@ -1,26 +1,65 @@
+# URL
+Brain_2_mart_export_brain=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Brain/Brain_2_mart_export_brain/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Cerebellum_2_mart_export_cerebellum=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Cerebellum/Cerebellum_2_mart_export_cerebellum/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Heart_2_Heart_Ensembl_NCBI_Crosswalk=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Kidney_2_mart_export_kidney=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Kidney/Kidney_2_mart_export_kidney/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Liver_2_mart_export_liver=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Liver/Liver_2_mart_export_liver/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Testis_2_mart_export_testis=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Testis/Testis_2_mart_export_testis/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Heart_2_Brain=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Organs/Heart_2_Brain/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Heart_2_Cerebellum=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Organs/Heart_2_Cerebellum/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Heart_2_Kidney=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Organs/Heart_2_Kidney/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Heart_2_Liver=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Organs/Heart_2_Liver/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
+Heart_2_Testis=https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Organs/Heart_2_Testis/ML_AdaBoost_StandardRecordBlocker/correspondences.csv
+
 # Brain
 mkdir -p data/correspondences/Brain/Brain_2_mart_export_brain
-wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-fusion/correspondences/Brain/Brain_2_mart_export_brain/correspondences.csv -O data/correspondences/Brain/Brain_2_mart_export_brain/correspondences.csv
+wget -N $Brain_2_mart_export_brain -O data/correspondences/Brain/Brain_2_mart_export_brain/correspondences.csv
 
 # Cerebellum
 mkdir -p data/correspondences/Cerebellum/Cerebellum_2_mart_export_cerebellum
-wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-fusion/correspondences/Cerebellum/Cerebellum_2_mart_export_cerebellum/correspondences.csv -O data/correspondences/Cerebellum/Cerebellum_2_mart_export_cerebellum/correspondences.csv
+wget -N $Cerebellum_2_mart_export_cerebellum -O data/correspondences/Cerebellum/Cerebellum_2_mart_export_cerebellum/correspondences.csv
 
 # Heart
 mkdir -p data/correspondences/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk
-wget - N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk/ML_AdaBoost_StandardRecordBlocker/correspondences.csv -O data/correspondences/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk/correspondences.csv
-mkdir -p data/correspondences/Heart/Heart_Ensembl_NCBI_Crosswalk_2_all_gene_disease_pmid_associations
+wget - N $Heart_2_Heart_Ensembl_NCBI_Crosswalk -O data/correspondences/Heart/Heart_2_Heart_Ensembl_NCBI_Crosswalk/correspondences.csv
+
 mkdir -p data/correspondences/Heart/mart_export_heart_2_Heart_Ensembl_NCBI_Crosswalk
-wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-fusion/correspondences/Heart/mart_export_heart_2_Heart_Ensembl_NCBI_Crosswalk/correspondences.csv -O data/correspondences/Heart/mart_export_heart_2_Heart_Ensembl_NCBI_Crosswalk/correspondences.csv
+# missing
 
 # Kidney
 mkdir -p data/correspondences/Kidney/Kidney_2_mart_export_kidney
-wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Kidney/Kidney_2_mart_export_kidney/ML_AdaBoost_StandardRecordBlocker/correspondences.csv -O data/correspondences/Kidney/Kidney_2_mart_export_kidney/correspondences.csv
+wget -N $Kidney_2_mart_export_kidney -O data/correspondences/Kidney/Kidney_2_mart_export_kidney/correspondences.csv
 
 # Liver
 mkdir -p data/correspondences/Liver/Liver_2_mart_export_liver
-wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Liver/Liver_2_mart_export_liver/ML_AdaBoost_StandardRecordBlocker/correspondences.csv -O data/correspondences/Liver/Liver_2_mart_export_liver/correspondences.csv
+wget -N $Liver_2_mart_export_liver -O data/correspondences/Liver/Liver_2_mart_export_liver/correspondences.csv
 
 # Testis
 mkdir -p data/correspondences/Testis/Testis_2_mart_export_testis
-wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/output/Testis/Testis_2_mart_export_testis/ML_AdaBoost_StandardRecordBlocker/correspondences.csv -O data/correspondences/Testis/Testis_2_mart_export_testis/correspondences.csv
+wget -N $Testis_2_mart_export_testis -O data/correspondences/Testis/Testis_2_mart_export_testis/correspondences.csv
+
+# Organs
+mkdir -p data/correspondences/Organs/Heart_2_Brain
+wget -N $Heart_2_Brain -O data/correspondences/Organs/Heart_2_Brain/correspondences.csv
+
+mkdir -p data/correspondences/Organs/Heart_2_Cerebellum
+wget -N $Heart_2_Cerebellum -O data/correspondences/Organs/Heart_2_Cerebellum/correspondences.csv
+
+mkdir -p data/correspondences/Organs/Heart_2_Kidney
+wget -N $Heart_2_Kidney -O data/correspondences/Organs/Heart_2_Kidney/correspondences.csv
+
+mkdir -p data/correspondences/Organs/Heart_2_Liver
+wget -N $Heart_2_Liver -O data/correspondences/Organs/Heart_2_Liver/correspondences.csv
+
+mkdir -p data/correspondences/Organs/Heart_2_Testis
+wget -N $Heart_2_Testis -O data/correspondences/Organs/Heart_2_Testis/correspondences.csv
