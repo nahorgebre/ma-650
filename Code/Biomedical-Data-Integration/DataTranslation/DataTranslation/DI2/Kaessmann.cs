e7721b1b97413de62a296e17b28eb05a227f03bd
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-02 04:18:47 +0100

    Update Kaessmann.cs

--- Code/Biomedical-Data-Integration/DataTranslation/DataTranslation/DI2/Kaessmann.cs
+++ Code/Biomedical-Data-Integration/DataTranslation/DataTranslation/DI2/Kaessmann.cs
@@ -14,22 +14,186 @@ namespace DataTranslation
 
             FileInfo file = new FileInfo(string.Format("{0}/data/input/DI2/kaessmann-fused.xml", Environment.CurrentDirectory));
 
-            List<Gene> gene_list = extractXml(file);
+            List<Gene> gene_list = parseXml(file, "Kaessmann_{0}_rid");
 
+            /*
             foreach (Gene geneItem in gene_list)
             {
                 
                 foreach (GeneName geneNameItem in geneItem.geneNames)
                 {
-                    
+
                     Console.WriteLine("Name: " + geneNameItem.name);
 
                 }
 
             }
+            */
 
             Methods.createXml(gene_list: gene_list, fileName: "Kaessmann_dt.xml", directory: "data/output/DI2");
             //Methods.createTsv(gene_list: gene_list, fileName: "Kaessmann_dt.tsv", directory: string.Format("{0}/data/input/DI2/", Environment.CurrentDirectory) );
+
+        }
+
+        public static List<Gene> parseXml(FileInfo file, String recordIdPattern)
+        {
+
+            List<Gene> geneList = new List<Gene>();
+
+            int counter = 1;
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.DtdProcessing = DtdProcessing.Parse;
+
+            using (XmlReader reader = XmlReader.Create(file.FullName, settings))
+            {
+                while (reader.ReadToFollowing("gene"))
+                {
+
+                    Gene gene = new Gene();
+
+                    gene.recordId = string.Format("Kaessmann_{0}_rid", counter);
+
+                    // ensemblId
+                    String ensemblId = string.Empty;
+                    reader.ReadToFollowing("ensemblId");
+                    ensemblId = reader.ReadElementContentAsString();
+
+                    gene.ensemblId = ensemblId;
+
+
+                    // ncbiId
+                    String ncbiId = string.Empty;
+                    reader.ReadToFollowing("ncbiId");
+                    ncbiId = reader.ReadElementContentAsString();
+
+                    gene.ncbiId = ncbiId;
+
+
+                    // geneDescriptions
+                    reader.ReadToFollowing("geneDescriptions"); 
+                    XmlReader geneDescriptionsInner = reader.ReadSubtree();
+                    while (geneDescriptionsInner.Read())
+                    {
+                    }
+
+
+                    // geneNames
+                    List<GeneName> geneNameList = new List<GeneName>();
+
+                    reader.ReadToFollowing("geneNames"); 
+                    XmlReader geneNamesInner = reader.ReadSubtree();
+                    while (geneNamesInner.Read())
+                    {
+
+                        GeneName geneName = new GeneName();
+
+                        geneNamesInner.ReadToFollowing("geneName");
+                        XmlReader geneNameInner = geneNamesInner.ReadSubtree();
+                        while (geneNameInner.Read())
+                        {
+
+                            String name = string.Empty;
+                            geneNameInner.ReadToFollowing("name");
+                            name = geneNameInner.ReadElementContentAsString();
+
+                            geneName.name = name;
+
+                        }
+
+                        geneNameList.Add(geneName);
+
+                    }
+
+                    gene.geneNames = geneNameList;
+
+
+                    // organs
+                    List<Organ> organList = new List<Organ>();
+
+                    reader.ReadToFollowing("organs"); 
+                    XmlReader organsInner = reader.ReadSubtree();
+                    while (organsInner.Read())
+                    {
+                        
+                        organsInner.ReadToFollowing("organ");
+                        XmlReader organInner = organsInner.ReadSubtree();
+                        while (organInner.Read())
+                        {
+
+                            Organ organ = new Organ();
+
+                            // organName
+                            String organName = string.Empty;
+                            organInner.ReadToFollowing("organName");
+                            organName = organInner.ReadElementContentAsString();
+
+                            organ.organName = organName;
+
+
+                            // disagreement
+                            String disagreement = string.Empty;
+                            organInner.ReadToFollowing("disagreement");
+                            disagreement = organInner.ReadElementContentAsString();
+
+                            organ.disagreement = disagreement;
+
+
+                            // probEqualOrthoAdj
+                            String probEqualOrthoAdj = string.Empty;
+                            organInner.ReadToFollowing("probEqualOrthoAdj");
+                            probEqualOrthoAdj = organInner.ReadElementContentAsString();
+
+                            organ.probEqualOrthoAdj = probEqualOrthoAdj;
+
+
+                            // call
+                            String call = string.Empty;
+                            organInner.ReadToFollowing("call");
+                            call = organInner.ReadElementContentAsString();
+
+                            organ.call = call;
+
+
+                            organList.Add(organ);
+
+                        }
+
+                    }
+
+                    gene.organs = organList;
+                    
+
+                    // publicationMentions
+                    reader.ReadToFollowing("publicationMentions"); 
+                    XmlReader publicationMentionsInner = reader.ReadSubtree();
+                    while (publicationMentionsInner.Read())
+                    {
+                    }
+
+                    // patentMentions
+                    reader.ReadToFollowing("patentMentions"); 
+                    XmlReader patentMentionsInner = reader.ReadSubtree();
+                    while (patentMentionsInner.Read())
+                    {
+                    }
+
+                    // diseaseAssociations
+                    reader.ReadToFollowing("diseaseAssociations"); 
+                    XmlReader diseaseAssociationsInner = reader.ReadSubtree();
+                    while (diseaseAssociationsInner.Read())
+                    {
+                    }
+
+                    geneList.Add(gene);
+
+                    counter++;
+
+                }
+
+            }
+
+            return geneList;
 
         }
 
