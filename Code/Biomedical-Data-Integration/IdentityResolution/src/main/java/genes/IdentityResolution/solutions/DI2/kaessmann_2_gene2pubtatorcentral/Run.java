8c09d99d99079ab3c7d1818a8e04f234ea43492c
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-04 17:15:04 +0100

    Update Run.java

--- Code/Biomedical-Data-Integration/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/DI2/kaessmann_2_gene2pubtatorcentral/Run.java
+++ Code/Biomedical-Data-Integration/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/DI2/kaessmann_2_gene2pubtatorcentral/Run.java
@@ -5,7 +5,7 @@ public class Run {
     public static void main( String[] args ) throws Exception
     {
         //LR_StandardRecordBlocker.main(args);
-        ML_StandardRecordBlocker.main(args);
+        ML_StandardRecordBlocker2.main(args);
     }
     
 }
