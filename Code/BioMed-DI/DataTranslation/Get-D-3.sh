a7dc306f84b2903375f8335a504d4a48867c4a3a
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-08 14:56:55 +0100

    Update BioMed-DI

--- Code/BioMed-DI/DataTranslation/Get-D-3.sh
+++ Code/BioMed-DI/DataTranslation/Get-D-3.sh
@@ -2,5 +2,6 @@
 
 mkdir -p data/input/DI4
 # kaessmann-publication-fused
+wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-fusion/output/kaessmann-fused.xml -O data/input/DI2/kaessmann-fused.xml
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-translation/input/Gene-Disease-Associations/all_gene_disease_pmid_associations.tsv -O data/input/Gene-Disease-Associations/all_gene_disease_pmid_associations.tsv
 wget -N https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-translation/input/Gene-Disease-Associations/readme.txt -O data/input/Gene-Disease-Associations/readme.txt
