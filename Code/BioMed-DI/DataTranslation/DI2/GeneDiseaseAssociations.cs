84d0cbd3993c67dbda65521ef5a9938e73ccccea
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-16 16:00:10 +0100

    Update GeneDiseaseAssociations.cs

--- Code/BioMed-DI/DataTranslation/DI2/GeneDiseaseAssociations.cs
+++ Code/BioMed-DI/DataTranslation/DI2/GeneDiseaseAssociations.cs
@@ -10,11 +10,121 @@ namespace DataTranslation
 
         // 7 output files - (1.548.061) / 7 = 221.151,6
 
-        public static int all_gene_disease_pmid_associations_partitionSize = 1087340;
+        public static int all_gene_disease_pmid_associations_partitionSize = 221151;
         
         public static int all_gene_disease_pmid_associations_partitionNumbers = 7;
 
+        public static void runDataTranslation()
+        {
 
+            Directory.CreateDirectory(string.Format("{0}/{1}", Environment.CurrentDirectory, DI2.outputDirectory)); 
+            
+            Console.WriteLine("Start all_gene_disease_pmid_associations_dt()");
+
+            for (int i = 1; i <= all_gene_disease_pmid_associations_partitionNumbers; i++)
+            {
+
+                List<Gene> gene_list = new List<Gene>();
+
+                using (var reader = new StreamReader(string.Format("{0}/{1}/{2}", Environment.CurrentDirectory, DI2.inputDirectory, "all_gene_disease_pmid_associations.tsv")))
+                {
+
+                    reader.ReadLine();
+
+                    int recordIdCounter = 1;
+
+                    int conditionCounter = 1;
+
+                    while (!reader.EndOfStream)
+                    {
+
+                        var line = reader.ReadLine();
+
+                        String[] values = line.Split('\t');
+
+                        // 0 - geneId 
+                        // 1 - geneSymbol  
+                        // 2 - DSI 
+                        // 3 - DPI 
+                        // 4 - diseaseId 
+                        // 5 - diseaseName
+                        // 6 - diseaseType
+                        // 7 - diseaseClass    
+                        // 8 - diseaseSemanticType 
+                        // 9 - score   
+                        // 10 - EI 
+                        // 11 - YearInitial 
+                        // 12 - YearFinal
+                        // 13 - pmid    
+                        // 14 - source
+
+                        bool condition;
+
+                        if (i == Variables.gene2pubtatorcentral_partitionNumbers)
+                        {
+
+                            condition = conditionCounter > Variables.gene2pubtatorcentral_partitionSize * (i - 1);
+
+                        }
+                        else
+                        {
+
+                            condition = (conditionCounter > Variables.gene2pubtatorcentral_partitionSize * (i - 1)) & (conditionCounter <= Variables.gene2pubtatorcentral_partitionSize * i);
+
+                        }
+
+                        if (condition)
+                        {
+
+
+                            Gene gene = new Gene();
+
+                            gene.recordId = string.Format("gene_disease_pmid_associations_{0}_rid", recordIdCounter);
+
+
+                            string ncbiId = values[0].Trim();
+
+                            if (!ncbiId.Equals(string.Empty))
+                            {
+
+                                gene.ncbiId = ncbiId;
+
+                            }
+
+
+                            string name = values[1].Trim();
+
+                            if (!name.Equals(string.Empty))
+                            {
+
+                                gene.geneNames = name;
+
+                            }
+
+                            gene.diseaseAssociations = getDiseaseAssociations(values);
+
+                            gene_list.Add(gene);
+
+                            recordIdCounter++;
+
+
+                        }
+
+                        conditionCounter++;
+                            
+                    }
+
+                }
+
+                Methods.createXmlGene(gene_list: gene_list, fileName: "all_gene_disease_pmid_associations_" + i + "_dt.xml", directory: DI2.outputDirectory);
+
+                Methods.createTsv(gene_list: gene_list, fileName: "all_gene_disease_pmid_associations_" + i + "_dt.tsv", directory: DI2.outputDirectory);
+
+            }
+
+        }
+
+        
         public static List<DiseaseAssociation> getDiseaseAssociations(String[] values)
         {
 
@@ -161,115 +271,6 @@ namespace DataTranslation
 
         }
 
-        public static void runDataTranslation()
-        {
-
-            Directory.CreateDirectory(string.Format("{0}/{1}", Environment.CurrentDirectory, DI2.outputDirectory)); 
-            
-            Console.WriteLine("Start all_gene_disease_pmid_associations_dt()");
-
-            for (int i = 1; i <= all_gene_disease_pmid_associations_partitionNumbers; i++)
-            {
-
-                List<Gene> gene_list = new List<Gene>();
-
-                using (var reader = new StreamReader(string.Format("{0}/{1}/{2}", Environment.CurrentDirectory, DI2.inputDirectory, "all_gene_disease_pmid_associations.tsv")))
-                {
-
-                    reader.ReadLine();
-
-                    int recordIdCounter = 1;
-
-                    int conditionCounter = 1;
-
-                    while (!reader.EndOfStream)
-                    {
-
-                        var line = reader.ReadLine();
-
-                        String[] values = line.Split('\t');
-
-                        // 0 - geneId 
-                        // 1 - geneSymbol  
-                        // 2 - DSI 
-                        // 3 - DPI 
-                        // 4 - diseaseId 
-                        // 5 - diseaseName
-                        // 6 - diseaseType
-                        // 7 - diseaseClass    
-                        // 8 - diseaseSemanticType 
-                        // 9 - score   
-                        // 10 - EI 
-                        // 11 - YearInitial 
-                        // 12 - YearFinal
-                        // 13 - pmid    
-                        // 14 - source
-
-                        bool condition;
-
-                        if (i == Variables.gene2pubtatorcentral_partitionNumbers)
-                        {
-
-                            condition = conditionCounter > Variables.gene2pubtatorcentral_partitionSize * (i - 1);
-
-                        }
-                        else
-                        {
-
-                            condition = (conditionCounter > Variables.gene2pubtatorcentral_partitionSize * (i - 1)) & (conditionCounter <= Variables.gene2pubtatorcentral_partitionSize * i);
-
-                        }
-
-                        if (condition)
-                        {
-
-
-                            Gene gene = new Gene();
-
-                            gene.recordId = string.Format("gene_disease_pmid_associations_{0}_rid", recordIdCounter);
-
-
-                            string ncbiId = values[0].Trim();
-
-                            if (!ncbiId.Equals(string.Empty))
-                            {
-
-                                gene.ncbiId = ncbiId;
-
-                            }
-
-
-                            string name = values[1].Trim();
-
-                            if (!name.Equals(string.Empty))
-                            {
-
-                                gene.geneNames = name;
-
-                            }
-
-                            gene.diseaseAssociations = getDiseaseAssociations(values);
-
-                            gene_list.Add(gene);
-
-                            recordIdCounter++;
-
-
-                        }
-
-                        conditionCounter++;
-                            
-                    }
-
-                }
-
-                Methods.createXmlGene(gene_list: gene_list, fileName: "all_gene_disease_pmid_associations_" + i + "_dt.xml", directory: DI2.outputDirectory);
-
-                Methods.createTsv(gene_list: gene_list, fileName: "all_gene_disease_pmid_associations_" + i + "_dt.tsv", directory: DI2.outputDirectory);
-
-            }
-
-        }
 
     }
 
