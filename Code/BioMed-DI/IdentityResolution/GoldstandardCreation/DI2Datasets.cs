f6aa8564432102613cde247b8cf192bed5ec9b2a
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-16 16:24:14 +0100

    Update BioMed-DI

--- Code/BioMed-DI/IdentityResolution/GoldstandardCreation/DI2Datasets.cs
+++ Code/BioMed-DI/IdentityResolution/GoldstandardCreation/DI2Datasets.cs
@@ -7,7 +7,14 @@ namespace GoldstandardCreation
 
         public static string kaessmann_path = Environment.CurrentDirectory + "/data/input/DI2/Kaessmann_dt.tsv";
 
-        public static string all_gene_disease_pmid_associations_path = Environment.CurrentDirectory + "/data/input/DI2/all_gene_disease_pmid_associations_dt.tsv";
+        public static string all_gene_disease_pmid_associations_path(int fileNumber) 
+        {
+
+            String path = Environment.CurrentDirectory + "/data/input/DI2/all_gene_disease_pmid_associations_" + fileNumber + "_dt.tsv";
+
+            return path;
+
+        }
 
     }
 
