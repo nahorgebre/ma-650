5aa3486a7d90c03ef68d4fc47edc577958433ac0
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-16 03:23:52 +0100

    Update Run-DF-1.sh

--- Code/BioMed-DI/DataFusion/Run-DF-1.sh
+++ Code/BioMed-DI/DataFusion/Run-DF-1.sh
@@ -1,5 +1,5 @@
 mvn compile
 
-mvn exec:java -Dexec.mainClass="genes.DataFusion.solution.DataFusion1"
+mvn exec:java -Dexec.mainClass="genes.DataFusion.solution.DI1DataFusion"
 
 mvn exec:java -Dexec.mainClass="genes.DataFusion.AWS.UploadToS3"
