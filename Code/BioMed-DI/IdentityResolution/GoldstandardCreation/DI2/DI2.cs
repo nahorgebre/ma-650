de2bf38802fab1c67688fa3eff2d6961d2a820de
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-16 16:30:57 +0100

    Update Bio-Med-DI

--- Code/BioMed-DI/IdentityResolution/GoldstandardCreation/DI2/DI2.cs
+++ Code/BioMed-DI/IdentityResolution/GoldstandardCreation/DI2/DI2.cs
@@ -7,24 +7,30 @@ namespace GoldstandardCreation
     class DI2
     {
 
+        public static int kaessmann_2_all_gene_disease_pmid_associations_partitionNumbers = 7;
+
         public static void runDataTranslation() 
         {
 
-            string comparison = "kaessmann_2_all_gene_disease_pmid_associations";
-            string directoryName = string.Format("{0}/data/output/DI2/{1}", Environment.CurrentDirectory, comparison);
-            string trueFile = string.Format("{0}/true.csv", directoryName);
-            string falseFile = string.Format("{0}/false.csv", directoryName);
-
-            Directory.CreateDirectory(directoryName);
-
-            if (!File.Exists(trueFile) | !File.Exists(falseFile))
+            for (int i = 1; i <= kaessmann_2_all_gene_disease_pmid_associations_partitionNumbers; i++)
             {
+                string comparison = "kaessmann_2_all_gene_disease_pmid_associations_" + i;
+                string directoryName = string.Format("{0}/data/output/DI2/{1}/{2}", Environment.CurrentDirectory, kaessmann_2_all_gene_disease_pmid_associations_partitionNumbers, comparison);
+                string trueFile = string.Format("{0}/true.csv", directoryName);
+                string falseFile = string.Format("{0}/false.csv", directoryName);
 
-                Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
-                (List<Goldstandard> trueList, List<Goldstandard> falseList) = Methods.compareFiles(DI2Datasets.kaessmann_path, DI2Datasets.all_gene_disease_pmid_associations_path, 3, Methods.GeneName);
-                Methods.createOuput(trueFile, trueList);
-                Methods.createOuput(falseFile, falseList);
+                Directory.CreateDirectory(directoryName);
 
+                if (!File.Exists(trueFile) | !File.Exists(falseFile))
+                {
+
+                    Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().Name);
+                    (List<Goldstandard> trueList, List<Goldstandard> falseList) = Methods.compareFiles(DI2Datasets.kaessmann_path, DI2Datasets.all_gene_disease_pmid_associations_path(i), 3, Methods.GeneName);
+                    Methods.createOuput(trueFile, trueList);
+                    Methods.createOuput(falseFile, falseList);
+
+                }
+                
             }
 
         }
