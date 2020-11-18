394bb06149d690c104e7ea406a09c500f658c525
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-18 14:08:09 +0100

    Update Program.cs

--- Code/BioMed-DI/DataFusion/DataPreparation/Program.cs
+++ Code/BioMed-DI/DataFusion/DataPreparation/Program.cs
@@ -8,7 +8,15 @@ namespace DataPreparation
         static void Main(string[] args)
         {
 
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
             all_gene_disease_pmid_associations.runDataPreparation();
+
+            watch.Stop();
+
+            double elapsedMinutes = TimeSpan.FromMilliseconds(watch.ElapsedMilliseconds).TotalMinutes;
+
+            Console.WriteLine("Time: " + elapsedMinutes);
             
         }
     }
