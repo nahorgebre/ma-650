ddedc8aace5dbb58d3a169206f55e354deacdbc9
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-20 18:21:09 +0100

    Update Get-D-3.sh

--- Code/BioMed-DI/DataTranslation/Get-D-3.sh
+++ Code/BioMed-DI/DataTranslation/Get-D-3.sh
@@ -2,4 +2,4 @@
 mkdir -p data/input/DI3
 
 wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-translation/input/Gene2Pubtatorcentral/gene2pubtatorcentral.tsv -O data/input/DI3/gene2pubtatorcentral.tsv
-wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-analysis/analysis1/analysis1.xml - O data/input/DI3/analysis1.xml
+wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/data-analysis/analysis1/analysis1.xml -O data/input/DI3/analysis1.xml
