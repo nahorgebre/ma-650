da736855b9260c64d640541a2f3bc0aff5a6dc1f
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-30 18:03:15 +0100

    Update PartitionNumbers.java

--- Code/BioMed-DI/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/PartitionNumbers.java
+++ Code/BioMed-DI/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/PartitionNumbers.java
@@ -2,8 +2,8 @@ package genes.IdentityResolution.solutions;
 
 public class PartitionNumbers {
 
-    public static int kaessmannDiseaseAssociations_2_gene2pubtatorcentral = 2; //50
+    public static int kaessmannDiseaseAssociations_2_gene2pubtatorcentral = 50; //50
 
-    public static int kaessmann_2_all_gene_disease_pmid_associations = 2; //7
+    public static int kaessmann_2_all_gene_disease_pmid_associations = 7; //7
     
 }
