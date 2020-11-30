b4aa95bd7f161866dd514a1ecdd0bb787a9b26ba
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-11-30 04:53:55 +0100

    Update BioMed-DI

--- Code/BioMed-NER/Scispacy/main.py
+++ Code/BioMed-NER/Scispacy/main.py
@@ -1,34 +1,24 @@
-#pip install scispacy
-#pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.3.0/en_ner_bionlp13cg_md-0.3.0.tar.gz
-#pip install spacy==2.3.2
-
 import spacy
 import scispacy
-import pandas as pd
-import csv
+import en_ner_bionlp13cg_md
 
-def getPatentAbstract(data_key):
-    patentAbstractList = []
-    with open(data_key, "r") as f:
-        reader = csv.reader(f, delimiter="\t")
-        for i, line in enumerate(reader):
-            if i > 0:
-                patentAbstractList.append( Patent.Patent(patentNumber = line[0], patentDate = line[1], patentAbstract = line[2]) )
-    return patentAbstractList
+import csv
 
 def getGeneNames(document):
     entityList = []
-    nlp = spacy.load("en_ner_bionlp13cg_md")
+    nlp = spacy.load('en_ner_bionlp13cg_md')
     for ent in nlp(document).ents:
         if ent.label_=='GENE_OR_GENE_PRODUCT':
             entityList.append(ent.text)
     return '|'.join(entityList)
 
-
-with open('title-output.tsv','wt') as f1:
-    writer = csv.writer(f1, delimiter='\t')
-    writer.writerow(['patentNumber', 'patentDate', 'genes'])
-    for line in getPatentAbstract('title.tsv'):
-        writer.writerow([line.patentNumber, line.patentDate, getGeneNames(line.patentAbstract)])
-
- 
+def createTitleOutputFile(inputFileName, outputFileName):
+    with open('data/output/' + outputFileName, 'w', newline='') as outputFile:
+        writer = csv.writer(outputFile, delimiter='\t')
+        writer.writerow(['patentNumber', 'patentDate', 'patentGeneNames'])
+        with open('data/input/' + inputFileName) as inputFile:
+            reader = csv.reader(inputFile, delimiter='\t')
+            for row in reader:
+                writer.writerow([row[0], row[1], getGeneNames(row[2])])
+            
+createTitleOutputFile('title.tsv', 'abstractGene_y1985.csv')
