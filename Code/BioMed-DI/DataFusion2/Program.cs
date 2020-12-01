894a63dd58db31a32533afe7692f7307bff1d4cc
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-01 16:58:18 +0100

    Update BioMed-DI

--- Code/BioMed-DI/DataFusion2/Program.cs
+++ Code/BioMed-DI/DataFusion2/Program.cs
@@ -21,20 +21,28 @@ namespace DataFusion2
                 else if (parameter.Equals("DI2"))
                 {
 
+                    AWSlistingContents.createCorrespondencesShellScriptOutput(parameter);
+
                 }
                 else if (parameter.Equals("DI3"))
-                {
+                {  
 
                     AWSlistingContents.createCorrespondencesShellScriptOutput(parameter);
+
+                    DI3DataFusion.run();
 
                 }
 
             }
+
+            /*
 
             for (int i = 1; i <= 50; i++)
             {
                 Console.WriteLine("new FileInfo(Environment.CurrentDirectory + \"/data/correspondences/DI3/kaessmannDiseaseAssociations_2_gene2pubtatorcentral_" + i + "/correspondences.csv\"),");
             }
+
+            */
 
         }
 
