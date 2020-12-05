9394615d6b50dc5569829e66444b9db3f9c85009
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-05 18:47:35 +0100

    Update BioMed-DI

--- Code/BioMed-NER/Scispacy/Get-D.sh
+++
@@ -1,7 +0,0 @@
-
-mkdir -p data/input
-wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/parser/output/title.tsv -O data/input/title.tsv
-wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/parser/output/abstract.tsv -O data/input/abstract.tsv
-wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/parser/output/claims.tsv -O data/input/claims.tsv
-wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/parser/output/description.tsv -O data/input/description.tsv
-mkdir -p data/output
