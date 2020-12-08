90ec31a45303ce68ed62af1c4e96b1d41da4d051
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-08 19:19:00 +0100

    Update Run.java

--- Code/BioMed-DI/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/DI1/Brain_2_Liver/Run.java
+++ Code/BioMed-DI/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/DI1/Brain_2_Liver/Run.java
@@ -5,7 +5,7 @@ public class Run {
     public static void main( String[] args ) throws Exception
     {
         
-        LR_StandardRecordBlocker.main(args);
+        //LR_StandardRecordBlocker.main(args);
 
         LR_SortedNeighbourhoodBlocker.main(args);
 
