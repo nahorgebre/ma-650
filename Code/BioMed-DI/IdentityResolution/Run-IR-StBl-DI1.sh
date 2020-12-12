cc32b52529732e32e88b8142a7a41301e13fd26d
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-12 19:06:17 +0100

    Update Run-IR-StBl-DI1.sh

---
+++ Code/BioMed-DI/IdentityResolution/Run-IR-StBl-DI1.sh
@@ -0,0 +1,47 @@
+mvn compile
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Heart_2_Brain.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Heart_2_Cerebellum.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Heart_2_Kidney.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Heart_2_Liver.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Heart_2_Testis.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Heart_2_Heart_Ensembl_NCBI_Crosswalk.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.mart_export_heart_2_Heart_Ensembl_NCBI_Crosswalk.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Cerebellum_2_Brain.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Cerebellum_2_Kidney.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Cerebellum_2_Liver.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Cerebellum_2_Testis.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Cerebellum_2_mart_export_cerebellum.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Brain_2_Kidney.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Brain_2_Liver.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Brain_2_Testis.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Brain_2_mart_export_brain.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Kidney_2_Liver.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Kidney_2_Testis.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Kidney_2_mart_export_kidney.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Testis_2_Liver.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Liver_2_mart_export_liver.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI1.Testis_2_mart_export_testis.LR_StandardRecordBlocker"
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.AWS.UploadToS3"
