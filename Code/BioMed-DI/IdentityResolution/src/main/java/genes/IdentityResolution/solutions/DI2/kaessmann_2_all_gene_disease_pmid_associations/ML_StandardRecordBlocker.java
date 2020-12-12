3e0d17885d1fa79d747b3325528ec0358e8b67fe
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-12 23:21:56 +0100

    Update ML_StandardRecordBlocker.java

--- Code/BioMed-DI/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/DI2/kaessmann_2_all_gene_disease_pmid_associations/ML_StandardRecordBlocker.java
+++ Code/BioMed-DI/IdentityResolution/src/main/java/genes/IdentityResolution/solutions/DI2/kaessmann_2_all_gene_disease_pmid_associations/ML_StandardRecordBlocker.java
@@ -99,11 +99,13 @@ public class ML_StandardRecordBlocker {
                 matchingRule.activateDebugReport(outputDirectory + "/debugResultsMatchingRule.csv", 1000);
     
                 // add comparators
+                /*
                 matchingRule.addComparator(new NcbiIdComperatorCosine());
                 matchingRule.addComparator(new NcbiIdComperatorJaccardOnNGrams());
                 matchingRule.addComparator(new NcbiIdComperatorLevenshtein());
                 matchingRule.addComparator(new NcbiIdComperatorSorensenDice());
                 matchingRule.addComparator(new NcbiIdComperatorTokenizingJaccard());
+                */
                 
                 matchingRule.addComparator(new GeneNameComperatorTokenizingJaccard());
                 matchingRule.addComparator(new GeneNameComperatorLowerCaseTokenizingJaccard());
