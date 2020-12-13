18877fb28ad137f32fb186ae81a35d0fedd72b97
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-13 14:47:48 +0100

    Update BioMed-DI

--- Code/BioMed-DI/IR-GS-Creation/Get-D-DI2.sh
+++ Code/BioMed-DI/IR-GS-Creation/Get-D-DI2.sh
@@ -1,7 +1,6 @@
 
 mkdir -p data/input/DI2
 wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/input/DI2/DI1_dt.tsv -O data/input/DI2/DI1_dt.tsv
-wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/input/DI2/Kaessmann_dt.tsv -O data/input/DI2/Kaessmann_dt.tsv
 wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/input/DI2/all_gene_disease_pmid_associations_1_dt.tsv -O data/input/DI2/all_gene_disease_pmid_associations_1_dt.tsv
 wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/input/DI2/all_gene_disease_pmid_associations_2_dt.tsv -O data/input/DI2/all_gene_disease_pmid_associations_2_dt.tsv
 wget https://nahorgebre-ma-650-master-thesis.s3.us-east-2.amazonaws.com/identity-resolution/input/DI2/all_gene_disease_pmid_associations_3_dt.tsv -O data/input/DI2/all_gene_disease_pmid_associations_3_dt.tsv
