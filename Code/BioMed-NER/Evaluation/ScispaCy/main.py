a4156749791cdc5d240c0f3b58a8e571edecde1d
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-20 16:04:08 +0100

    Update main.py

--- Code/BioMed-NER/Evaluation/ScispaCy/main.py
+++ Code/BioMed-NER/Evaluation/ScispaCy/main.py
@@ -24,6 +24,7 @@ def createOutputFile(inputFileName, outputFileName):
             reader = csv.reader(inputFile, delimiter='\t')
             for row1 in reader:
                 for row2 in getGeneNames(row1[1]).split('#'):
+                    print(row2)
                     print(len(row2))
                     writer.writerow([ row1[0], row2.split('|')[0], row2.split('|')[1], row2.split('|')[2] ])
 
