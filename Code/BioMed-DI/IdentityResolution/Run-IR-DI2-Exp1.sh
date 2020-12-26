0fe08a7efcfbc6532d6893adae80d7c8dc4c3c20
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2020-12-26 16:54:39 +0100

    Update Run-IR-DI2-Exp1.sh

--- Code/BioMed-DI/IdentityResolution/Run-IR-DI2-Exp1.sh
+++ Code/BioMed-DI/IdentityResolution/Run-IR-DI2-Exp1.sh
@@ -7,3 +7,5 @@ mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI2.experimen
 mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI2.experiments.Cosine_StandardRecordBlocker" -Dexec.args="0.9"  
 
 mvn exec:java -Dexec.mainClass="genes.IdentityResolution.solutions.DI2.experiments.SorensenDice_StandardRecordBlocker" -Dexec.args="0.9"  
+
+mvn exec:java -Dexec.mainClass="genes.IdentityResolution.AWS.UploadToS3"
