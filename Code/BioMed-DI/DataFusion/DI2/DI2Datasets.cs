68dd9945558a71b435611639ea09e4cdaa93a4b3
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2021-01-08 03:50:25 +0100

    Update BioMed-DI

--- Code/BioMed-DI/DataFusion/DI2/DI2Datasets.cs
+++ Code/BioMed-DI/DataFusion/DI2/DI2Datasets.cs
@@ -16,6 +16,7 @@ namespace DataFusion
 
             datasetFileInfoList.Add(new FileInfo(Environment.CurrentDirectory + "/data/input/DI4/patent_abstract_dt.xml"));
 
+            /*
             for (int i = 1; i <= 7; i++)
             {
 
@@ -29,6 +30,7 @@ namespace DataFusion
                 datasetFileInfoList.Add(new FileInfo(Environment.CurrentDirectory + "/data/input/DI3/gene2pubtatorcentral_" + i + "_dt.xml"));
 
             }
+            */
 
             return datasetFileInfoList;
 
