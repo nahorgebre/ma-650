0ba4e55aca3722142df9205213c7429690c42501
Nahor Gebretensae <nahor.gebretensae@outlook.com>
2021-01-08 19:17:03 +0100

    Update DI4Datasets.cs

--- Code/BioMed-DI/IR-GS-Creation/DI4/DI4Datasets.cs
+++ Code/BioMed-DI/IR-GS-Creation/DI4/DI4Datasets.cs
@@ -6,7 +6,7 @@ namespace IR_GS_Creation
     class DI4Datasets
     {
 
-        public static FileInfo DI3_dt = new FileInfo(Environment.CurrentDirectory + "/data/input/DI4/DI2_dt.tsv");  
+        public static FileInfo DI3_dt = new FileInfo(Environment.CurrentDirectory + "/data/input/DI4/DI3_dt.tsv");  
         public static FileInfo patent_abstract_dt = new FileInfo(Environment.CurrentDirectory + "/data/input/DI4/patent_abstract_dt.tsv");  
         public static FileInfo patent_title_dt = new FileInfo(Environment.CurrentDirectory + "/data/input/DI4/patent_title_dt.tsv");              
 
